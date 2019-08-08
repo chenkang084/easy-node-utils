818ed78d312a0474c581de2ebea002df67a746d9
kang.chen <kang.chen@coupang.com>
2019-08-08 16:16:34 +0800

    add string trim

--- lib/String.util.d.ts
+++ lib/String.util.d.ts
@@ -1,1 +1,2 @@
 export declare const isEmpty: (paramter: string) => void;
+export declare const trim: (paramter: string) => string;
--- src/String.util.ts
+++ src/String.util.ts
@@ -1,1 +1,5 @@
 export const isEmpty = (paramter: string) => {};
+
+export const trim = (paramter: string) => {
+  return paramter.replace(/\s+/g, '');
+};
